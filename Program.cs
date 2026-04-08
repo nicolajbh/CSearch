4d5942a83bf89d497cdca73281bd96d237243a62
Nicolaj Hansen <nicolaj_95@outlook.com>
2026-04-08 12:14:31 +0200

    refurb scrape

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+  private static readonly HttpClient client = new HttpClient();
+  static async Task Main(string[] args)
+  {
+    string baseUrl = "https://www.refurbed.dk/";
+    string response = await client.GetStringAsync(baseUrl);
+    Console.WriteLine("Response received");
+    Console.WriteLine(response);
+  }
+}
