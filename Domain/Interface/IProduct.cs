8835f2e13fe1b71bc2d7136beab2fbcd0e6ffa23
Oliver Mosgaard <dumbcaesar94@gmail.com>
2026-04-09 08:33:19 +0200

    models for scraping and products

---
+++ Domain/Interface/IProduct.cs
@@ -0,0 +1,9 @@
+﻿namespace CSearch.Domain.Interface;
+
+public interface IProduct
+{
+    string Site { get; }
+    string Name { get; }
+    string Price { get; }
+    string Page { get; }
+}
---
+++ Domain/Interface/IScrapeJob.cs
@@ -0,0 +1,11 @@
+﻿namespace CSearch.Domain.Interface;
+
+public interface IScrapeJob
+{
+    string SiteName { get; }
+    string BaseUrl { get; }
+    string QueryParams { get; }
+    string CardSelector { get; }
+    string NameSelector { get; }
+    string PriceSelector { get; }
+}
